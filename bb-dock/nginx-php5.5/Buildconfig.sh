0ad4eb87b6242442839321a8972f3018e9140e6a
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2014-10-07 21:24:37 +0200

    Added nginx repo, refactored nginx-php5.5 to use the new repo as base image.

--- bb-dock/nginx-php5.5/Buildconfig.sh
+++ bb-dock/nginx-php5.5/Buildconfig.sh
@@ -1,15 +1,13 @@
 #
 # build config
 #
-PACKAGES="nginx dev-lang/php:5.5 dev-php/xdebug"
+PACKAGES="dev-lang/php:5.5 dev-php/xdebug"
 
 #
 # this method runs in the bb builder container just before starting the build of the rootfs
 # 
 configure_rootfs_build()
 {
-    echo 'NGINX_MODULES_HTTP="access auth_basic autoindex charset fastcgi gzip gzip_static limit_req map proxy rewrite scgi ssi stub_status"' >> /etc/portage/make.conf
-    echo 'NGINX_MODULES_MAIL=""' >> /etc/portage/make.conf
     echo 'PHP_TARGETS="php5-5"' >> /etc/portage/make.conf
     echo 'PHP_INI_VERSION="production"' >> /etc/portage/make.conf
 
---
+++ bb-dock/nginx/Buildconfig.sh
@@ -0,0 +1,21 @@
+#
+# build config
+#
+PACKAGES="www-servers/nginx"
+
+#
+# this method runs in the bb builder container just before starting the build of the rootfs
+# 
+configure_rootfs_build()
+{
+    echo 'NGINX_MODULES_HTTP="access auth_basic autoindex charset fastcgi gzip gzip_static limit_req map proxy rewrite scgi ssi stub_status"' >> /etc/portage/make.conf
+    echo 'NGINX_MODULES_MAIL=""' >> /etc/portage/make.conf
+}
+
+#
+# this method runs in the bb builder container just before tar'ing the rootfs
+# 
+finish_rootfs_build()
+{
+    :
+}
