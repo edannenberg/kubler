402e30c54d194f910f2d8796add4a581153c7f90
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2014-10-21 00:06:09 +0200

    Handle https forwarding to php when using proxy. Add pecl-{memcache,apcu,redis}.

--- bb-dock/nginx-php5.5/Buildconfig.sh
+++ bb-dock/nginx-php5.5/Buildconfig.sh
@@ -1,7 +1,7 @@
 #
 # build config
 #
-PACKAGES="dev-lang/php:5.5 dev-php/xdebug"
+PACKAGES="dev-lang/php:5.5 dev-php/xdebug dev-php/pecl-memcache dev-php/pecl-redis dev-php/pecl-apcu"
 
 #
 # this method runs in the bb builder container just before starting the build of the rootfs
@@ -13,7 +13,7 @@ configure_rootfs_build()
 
     echo 'dev-lang/php bcmath calendar curl curlwrappers fpm gd mhash mysql mysqli pdo soap sockets xmlreader xmlrpc xmlwriter xpm xsl zip' > /etc/portage/package.use/php
     echo 'app-admin/eselect-php fpm' >> /etc/portage/package.use/php
-    echo 'dev-lang/pecl-apcu mmap' > /etc/portage/package.use/apcu
+    echo 'dev-php/pecl-apcu mmap' > /etc/portage/package.use/apcu
 }
 
 #
