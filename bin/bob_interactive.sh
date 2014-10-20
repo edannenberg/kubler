0c363b5091d4b70221d09ed7272078c31dbd0a15
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2014-10-21 00:16:05 +0200

    Mount given image config dir.

--- bin/bob_interactive.sh
+++ bin/bob_interactive.sh
@@ -2,10 +2,22 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo -e "starting interactive build container..\n"
+if [ -z $1 ]; then
+    echo "usage: bob-interactive-sh image"
+    exit 1
+fi
+
+REPO=$(realpath -s $SCRIPT_DIR/../bb-dock/${1})
+if [ ! -d $REPO ]; then
+    echo "error: can't find ${1} in bb-dock"
+    exit 1
+fi
+
+echo -e "starting interactive build container with ${1} config..\n\nto start the build run: build-root ${1}\n"
 
 docker run -it --rm \
---volumes-from portage-data \
--v $(realpath -s $SCRIPT_DIR/../tmp/distfiles):/distfiles \
--v $(realpath -s $SCRIPT_DIR/../tmp/packages):/packages \
-"gentoobb/bob:latest"
+    --volumes-from portage-data \
+    -v $(realpath -s $SCRIPT_DIR/../tmp/distfiles):/distfiles \
+    -v $(realpath -s $SCRIPT_DIR/../tmp/packages):/packages \
+    -v ${REPO}):/config \
+    "gentoobb/bob:latest"
