688b82a13e325ca47a3186bd8e1151a1e4ac0d2c
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2014-11-06 19:04:34 +0100

    Mask ruby20, enable ruby21. Resolves dead locks in the log-collector image.

--- bb-dock/ruby/Buildconfig.sh
+++ bb-dock/ruby/Buildconfig.sh
@@ -9,6 +9,14 @@ PACKAGES="dev-lang/ruby"
 configure_rootfs_build()
 {
     sed -i /^app-shells\\/bash/d /etc/portage/profile/package.provided
+    echo 'RUBY_TARGETS="ruby19 ruby21"' >> /etc/portage/make.conf
+    echo 'dev-lang/ruby ~amd64' >> /etc/portage/package.keywords/ruby
+    echo '=dev-ruby/racc-1.4.12 ~amd64' >> /etc/portage/package.keywords/ruby
+    echo '=virtual/rubygems-7 ~amd64' >> /etc/portage/package.keywords/ruby
+    echo '=dev-ruby/rdoc-4.1.2 ~amd64' >> /etc/portage/package.keywords/ruby
+    echo '=dev-ruby/rubygems-2.2.2 ~amd64' >> /etc/portage/package.keywords/ruby
+    echo '=dev-ruby/json-1.8.1 ~amd64' >> /etc/portage/package.keywords/ruby
+    echo '=dev-ruby/rake-10.3.2 ~amd64' >> /etc/portage/package.keywords/ruby
 }
 
 #
--- bb-dock/ruby/README.md
+++ bb-dock/ruby/README.md
@@ -2,7 +2,7 @@ Run this [Ruby][] image with:
 
     $ docker run -it --rm gentoobb/ruby
 
-Comes with ruby 1.9 and 2.0. RUN eselect ruby set 1|2 in your dockerbuild to switch between versions. Default is ruby 2.0.
+Comes with ruby 1.9 and 2.1. RUN eselect ruby set ruby19|ruby21 in your dockerbuild to switch between versions. Default is ruby 2.1.
 
 If you need to compile native ruby extensions use gentoobb/ruby-gcc.
 
