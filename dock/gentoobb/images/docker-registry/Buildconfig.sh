4176fce01e9743573422bd9fdd75cc7325aab347
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2015-01-19 00:52:18 +0100

    Update to docker-registry 0.9.1

--- dock/gentoobb/images/docker-registry/Buildconfig.sh
+++ dock/gentoobb/images/docker-registry/Buildconfig.sh
@@ -1,8 +1,9 @@
 #
 # build config
 #
-PACKAGES="dev-python/blinker dev-python/boto dev-python/backports-lzma dev-python/flask dev-python/flask-cors dev-python/gevent dev-python/pyyaml dev-python/redis-py dev-python/requests dev-python/rsa dev-python/simplejson dev-python/sqlalchemy"
+PACKAGES="dev-python/m2crypto dev-python/blinker dev-python/boto dev-python/backports-lzma dev-python/flask dev-python/flask-cors dev-python/gevent dev-python/pyyaml dev-python/redis-py dev-python/requests dev-python/rsa dev-python/simplejson dev-python/sqlalchemy"
 #KEEP_HEADERS=true
+REGISTRY_VERSION=0.9.1
 
 #
 # this method runs in the bb builder container just before starting the build of the rootfs
@@ -31,17 +32,18 @@ configure_rootfs_build()
 finish_rootfs_build()
 {
     # prepare docker-registry, final setup in Dockerfile
-    wget http://github.com/docker/docker-registry/archive/0.9.0.tar.gz
-    tar xzvf 0.9.0.tar.gz
-    mv docker-registry-0.9.0 $EMERGE_ROOT/docker-registry
+    wget http://github.com/docker/docker-registry/archive/${REGISTRY_VERSION}.tar.gz
+    tar xzvf ${REGISTRY_VERSION}.tar.gz
+    mv docker-registry-${REGISTRY_VERSION} $EMERGE_ROOT/docker-registry
     cp --no-clobber $EMERGE_ROOT/docker-registry/config/config_sample.yml $EMERGE_ROOT/docker-registry/config/config.yml
     # Disable strict dependencies (see dotcloud/docker-registry#466)
     sed -i 's/\(install_requires=\)/#\1/' $EMERGE_ROOT/docker-registry/setup.py \
         $EMERGE_ROOT/docker-registry/depends/docker-registry-core/setup.py
     log_as_installed "pip install" "gunicorn" "http://gunicorn.org/"
-    log_as_installed "manual install" "docker-registry-0.9.0" "http://github.com/docker/docker-registry/"
+    log_as_installed "manual install" "docker-registry-${REGISTRY_VERSION}" "http://github.com/docker/docker-registry/"
     # remove packages that were only needed at build time
     emerge -C dev-lang/python dev-lang/python-exec dev-python/setuptools
+    rm $EMERGE_ROOT/usr/lib64/libpython2.7.*
     # reflect uninstall in docs
     sed -i /^dev-lang\\/python/d "${DOC_PACKAGE_INSTALLED}"
     sed -i /^dev-python\\/setuptools/d "${DOC_PACKAGE_INSTALLED}"
