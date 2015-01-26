b1f41c15c316900fd4f742684ff113679f1d581e
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2015-01-26 07:56:15 +0100

    Update bob_interactive.sh so it supports non-docker build containers

--- bin/bob_interactive.sh
+++ bin/bob_interactive.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 if [ -z $1 ]; then
-    echo "usage: bob-interactive-sh image"
+    echo "usage: bob-interactive-sh <namespace/image>"
     exit 1
 fi
 
-REPO=$(realpath -s $SCRIPT_DIR/../dock/${1/\//\/images\/})
-if [ ! -d $REPO ]; then
-    echo "error: can't find ${REPO}"
-    exit 1
-fi
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_ROOT=$(realpath -s $SCRIPT_DIR/../)
+REPO=${1}
+REPO_DIR=$(realpath -s $SCRIPT_DIR/../dock/${REPO/\//\/images\/})
+[ ! -d $REPO_DIR ] && echo "error: can't find ${REPO_DIR}" && exit 1
 
-echo -e "starting interactive build container with bb-dock/${1} mounted as /config..\n\nto start the build run: build-root ${1}\n"
+[ ! -f "${PROJECT_ROOT}/inc/core.sh" ] && echo "error: failed to read ${PROJECT_ROOT}/inc/core.sh" && exit 1
+source "${PROJECT_ROOT}/inc/core.sh"
 
-docker run -it --rm --hostname bob-$1 \
-    -v $(realpath -s $SCRIPT_DIR/../tmp/distfiles):/distfiles \
-    -v $(realpath -s $SCRIPT_DIR/../tmp/packages):/packages \
-    -v ${REPO}:/config \
-    "gentoobb/bob:latest"
+[[ ! -f "${PROJECT_ROOT}/build.conf" ]] && die "error: failed to read build.conf"
+source "${PROJECT_ROOT}/build.conf"
+
+DATE_ROOT="${DATE?Error \$DATE is not defined.}"
+
+cd ${PROJECT_ROOT}/${REPO_PATH}
+source_namespace_conf ${REPO}
+validate_engine
+
+BUILDER=$(get_build_container ${REPO} ${IMAGE_PATH})
+[[ $? == 1 ]] && die "error while executing get_image_builder(): ${BUILDER}"
+
+image_exists "${BUILDER}" "${IMAGE_PATH}" || die "error could not find image ${BUILDER}"
+
+# pass variables starting with BOB_ to build container as ENV
+for bob_var in ${!BOB_*}; do
+    CONTAINER_ENV+=("${bob_var}=${!bob_var}")
+done
+
+CONTAINER_MOUNTS=(
+"$(realpath -s $SCRIPT_DIR/../tmp/distfiles):/distfiles"
+"$(realpath -s $SCRIPT_DIR/../tmp/packages):/packages"
+"${REPO_DIR}:/config"
+)
+
+CONTAINER_CMD=("/bin/bash")
+
+msg "using: ${CONTAINER_ENGINE} / builder: ${BUILDER}"
+echo -e "\nrunning interactive build container with ${REPO_DIR} mounted as /config\nartifacts from previous builds: /backup-rootfs\n"
+echo -e "to start the build: $ build-root ${REPO}"
+echo -e "*** if you plan to run emerge manually, source /etc/profile first ***\n"
+
+run_image "${BUILDER}" "${BUILDER}" "true"
