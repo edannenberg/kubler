f0d95b0ead2f6c4a9d2fd5eb947406b1cfe28928
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2015-04-13 06:20:58 +0200

    Added nodejs image

---
+++ dock/gentoobb/images/nodejs/Buildconfig.sh
@@ -0,0 +1,20 @@
+#
+# build config
+#
+PACKAGES="net-libs/nodejs"
+
+#
+# this method runs in the bb builder container just before starting the build of the rootfs
+#
+configure_rootfs_build()
+{
+    :
+}
+
+#
+# this method runs in the bb builder container just before tar'ing the rootfs
+#
+finish_rootfs_build()
+{
+    copy_gcc_libs
+}
---
+++ dock/gentoobb/images/nodejs/README.md
@@ -0,0 +1,8 @@
+Run this [nodejs][] image with:
+
+    $ docker run -it gentoobb/nodejs
+
+Includes [npm][]
+
+[nodejs]: https://nodejs.org/
+[npm]: https://docs.npmjs.com/
