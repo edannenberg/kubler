95dd86c4ba7eb11dcba6c34fc89194f3f0ad0524
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2015-06-14 16:06:47 +0200

    Update ruby deps

--- dock/gentoobb/images/ruby-gcc/Buildconfig.sh
+++ dock/gentoobb/images/ruby-gcc/Buildconfig.sh
@@ -19,8 +19,8 @@ configure_rootfs_build()
     update_keywords '=dev-ruby/power_assert-0.2.2' '+~amd64'
     update_keywords '=dev-ruby/minitest-5.5.1' '+~amd64'
     update_keywords '=dev-ruby/test-unit-3.0.9-r1' '+~amd64'
-    update_keywords '=virtual/rubygems-10' '+~amd64'
-    update_keywords '=dev-ruby/json-1.8.2-r1' '+~amd64'
+    update_keywords '=virtual/rubygems-11' '+~amd64'
+    update_keywords '=dev-ruby/json-1.8.3' '+~amd64'
 }
 
 #
