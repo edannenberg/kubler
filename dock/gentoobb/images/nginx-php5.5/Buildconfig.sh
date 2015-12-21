6ddeb2e775c5b02d10a5bc7e576d3c18b8473fc1
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2015-12-21 11:44:17 +0100

    Refactor php build, preparation for php7. Bump adminer version to 4.2.3.

--- dock/gentoobb/images/nginx-php5.5/Buildconfig.sh
+++ dock/gentoobb/images/nginx-php5.5/Buildconfig.sh
@@ -1,7 +1,9 @@
 #
 # build config
 #
-PACKAGES="dev-lang/php:5.5 dev-php/xdebug dev-php/pecl-memcache dev-php/pecl-redis dev-php/pecl-apcu pecl-imagick"
+PHP_SLOT="5.5"
+PHP_TARGET="php${PHP_SLOT/\./-}"
+PACKAGES="dev-lang/php:${PHP_SLOT} dev-php/xdebug dev-php/pecl-memcache dev-php/pecl-redis dev-php/pecl-apcu pecl-imagick"
 PHP_TIMEZONE="${BOB_TIMEZONE:-UTC}"
 #ICONV_FROM=gentoobb/glibc
 
@@ -10,11 +12,11 @@ PHP_TIMEZONE="${BOB_TIMEZONE:-UTC}"
 #
 configure_rootfs_build()
 {
-    echo 'PHP_TARGETS="php5-5"' >> /etc/portage/make.conf
+    echo "PHP_TARGETS=\"${PHP_TARGET}\"" >> /etc/portage/make.conf
     echo 'PHP_INI_VERSION="production"' >> /etc/portage/make.conf
 
     update_use 'dev-lang/php' '+bcmath' '+calendar' '+curl' '+curlwrappers' '+fpm' '+gd' '+mhash' \
-               '+mysql' '+mysqli' '+pdo' '+soap' '+sockets' '+xmlreader' '+xmlrpc' '+xmlwriter' '+xpm' '+xslt' '+zip'
+               '+mysql' '+mysqli' '+pcntl' '+pdo' '+soap' '+sockets' '+xmlreader' '+xmlrpc' '+xmlwriter' '+xpm' '+xslt' '+zip'
     update_use 'app-eselect/eselect-php' '+fpm'
     update_use 'dev-php/pecl-apcu' '+mmap'
 
@@ -30,16 +32,16 @@ configure_rootfs_build()
 finish_rootfs_build()
 {
     # set php iconv default to UTF-8, if you need full iconv functionality set ICONV_FROM=gentoobb/glibc above
-    sed -i 's/^;iconv.input_encoding = ISO-8859-1/iconv.input_encoding = UTF-8/g' $EMERGE_ROOT/etc/php/fpm-php5.5/php.ini
-    sed -i 's/^;iconv.internal_encoding = ISO-8859-1/iconv.internal_encoding = UTF-8/g' $EMERGE_ROOT/etc/php/fpm-php5.5/php.ini
-    sed -i 's/^;iconv.output_encoding = ISO-8859-1/iconv.output_encoding = UTF-8/g' $EMERGE_ROOT/etc/php/fpm-php5.5/php.ini
+    sed -i 's/^;iconv.input_encoding = ISO-8859-1/iconv.input_encoding = UTF-8/g' $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/php.ini
+    sed -i 's/^;iconv.internal_encoding = ISO-8859-1/iconv.internal_encoding = UTF-8/g' $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/php.ini
+    sed -i 's/^;iconv.output_encoding = ISO-8859-1/iconv.output_encoding = UTF-8/g' $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/php.ini
     # set php time zone
-    sed -i "s@^;date.timezone =@date.timezone = $PHP_TIMEZONE@g" $EMERGE_ROOT/etc/php/fpm-php5.5/php.ini
+    sed -i "s@^;date.timezone =@date.timezone = $PHP_TIMEZONE@g" $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/php.ini
     # use above changes also for php cli config
-    cp $EMERGE_ROOT/etc/php/fpm-php5.5/php.ini $EMERGE_ROOT/etc/php/cli-php5.5/php.ini
+    cp $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/php.ini $EMERGE_ROOT/etc/php/cli-php${PHP_SLOT}/php.ini
     # disable xdebug
-    rm $EMERGE_ROOT/etc/php/fpm-php5.5/ext-active/xdebug.ini
-    rm $EMERGE_ROOT/etc/php/cli-php5.5/ext-active/xdebug.ini
+    rm $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/ext-active/xdebug.ini
+    rm $EMERGE_ROOT/etc/php/cli-php${PHP_SLOT}/ext-active/xdebug.ini
     # required by null-mailer
     copy_gcc_libs
     chmod 0640 $EMERGE_ROOT/etc/nullmailer/remotes
@@ -49,7 +51,7 @@ finish_rootfs_build()
     find /usr/lib64/gcc/x86_64-pc-linux-gnu -name libgomp.so.* -exec cp {} $EMERGE_ROOT/usr/lib64/ \;
     # prepare adminer / phpinfo micro sites
     mkdir -p $EMERGE_ROOT/var/www/{adminer,phpinfo}
-    wget -O $EMERGE_ROOT/var/www/adminer/adminer.php https://downloads.sourceforge.net/adminer/adminer-4.2.2-en.php
+    wget -O $EMERGE_ROOT/var/www/adminer/adminer.php https://www.adminer.org/static/download/4.2.3/adminer-4.2.3-en.php
     wget -O $EMERGE_ROOT/var/www/adminer/adminer.css https://raw.github.com/vrana/adminer/master/designs/bueltge/adminer.css
     echo "<?php phpinfo(); ?>" > $EMERGE_ROOT/var/www/phpinfo/phpinfo.php
 }
