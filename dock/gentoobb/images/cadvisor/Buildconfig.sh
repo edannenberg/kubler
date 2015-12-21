4f0801eabcb4669864f4c64ab85d62b833aabab6
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2015-12-21 11:34:48 +0100

    Bump cadvisor version to 0.20.1 and fix the build

--- dock/gentoobb/images/cadvisor/Buildconfig.sh
+++ dock/gentoobb/images/cadvisor/Buildconfig.sh
@@ -2,7 +2,7 @@
 # build config
 #
 PACKAGES=""
-CADVISOR_VERSION="0.19.3"
+CADVISOR_VERSION="0.20.1"
 
 configure_bob()
 {
@@ -17,11 +17,27 @@ configure_bob()
     # occasionally fetch might fail due to exceeded rate limits from github, lets retry up to 5 times before giving up
     echo "fetching deps.."
     for i in {1..5}; 
-        do go get -d github.com/google/cadvisor && break || (echo "retry fetch.." && sleep 5); done
-    for i in {1..5}; 
         do go get github.com/tools/godep && break || (echo "retry fetch.." && sleep 5); done
     echo "building cadvisor.."
-    godep go build .
+
+    repo_path="github.com/google/cadvisor"
+
+    version=$( cat version/VERSION )
+    revision=$( git rev-parse --short HEAD 2> /dev/null || echo 'unknown' )
+    branch=$( git rev-parse --abbrev-ref HEAD 2> /dev/null || echo 'unknown' )
+    host=$( hostname -f )
+    build_date=$( date +%Y%m%d-%H:%M:%S )
+    go_version=$( go version | sed -e 's/^[^0-9.]*\([0-9.]*\).*/\1/' )
+
+    ldflags="
+     -X ${repo_path}/version.Version ${version}
+     -X ${repo_path}/version.Revision ${revision}
+     -X ${repo_path}/version.Branch ${branch}
+     -X ${repo_path}/version.BuildUser ${USER}@${host}
+     -X ${repo_path}/version.BuildDate ${build_date}
+     -X ${repo_path}/version.GoVersion ${go_version}"
+
+    godep go build -ldflags "${ldflags}" -o cadvisor ${repo_path}
     echo "done."
 
     mkdir -p ${EMERGE_ROOT}/bin
