789daaf27a1b320ce2c9f1f13e2d7062ec5a105b
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2016-01-18 01:36:05 +0100

    Add http2 support to nginx images

--- dock/gentoobb/images/nginx/Buildconfig.sh
+++ dock/gentoobb/images/nginx/Buildconfig.sh
@@ -9,8 +9,10 @@ PACKAGES="www-servers/nginx"
 configure_rootfs_build()
 {
     echo 'NGINX_MODULES_HTTP="access auth_basic autoindex charset fastcgi \
-             gzip gzip_static limit_req map proxy rewrite scgi spdy ssi stub_status"' >> /etc/portage/make.conf
+             gzip gzip_static limit_req map proxy rewrite scgi ssi stub_status"' >> /etc/portage/make.conf
     echo 'NGINX_MODULES_MAIL=""' >> /etc/portage/make.conf
+    update_keywords 'www-servers/nginx' '+~amd64'
+    update_use 'www-servers/nginx' '+http2'
     update_use 'dev-libs/libpcre' '-readline'
 }
 
