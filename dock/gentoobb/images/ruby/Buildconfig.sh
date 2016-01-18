7174d306cef9483550c02514dee1a5749a18fae5
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2016-01-18 01:40:41 +0100

    Bump ruby version to 2.3.0

--- dock/gentoobb/images/ruby/Buildconfig.sh
+++ dock/gentoobb/images/ruby/Buildconfig.sh
@@ -8,18 +8,20 @@ PACKAGES="dev-libs/gmp app-eselect/eselect-ruby dev-lang/ruby"
 #
 configure_rootfs_build()
 {
-    echo 'RUBY_TARGETS="ruby22"' >> /etc/portage/make.conf
+    echo 'RUBY_TARGETS="ruby23"' >> /etc/portage/make.conf
     update_keywords 'dev-lang/ruby' '+~amd64'
     update_keywords '=app-eselect/eselect-ruby-20141227' '+~amd64'
-    update_keywords '=dev-ruby/racc-1.4.12' '+~amd64'
-    update_keywords '=dev-ruby/rdoc-4.1.2' '+~amd64'
-    update_keywords '=dev-ruby/rubygems-2.4.8' '+~amd64'
+    update_keywords '=dev-ruby/racc-1.4.14' '+~amd64'
+    update_keywords '=dev-ruby/rdoc-4.2.1' '+~amd64'
+    update_keywords '=dev-ruby/rubygems-2.5.1' '+~amd64'
     update_keywords '=dev-ruby/rake-10.4.2' '+~amd64'
     update_keywords '=dev-ruby/power_assert-0.2.6' '+~amd64'
-    update_keywords '=dev-ruby/minitest-5.5.1' '+~amd64'
-    update_keywords '=dev-ruby/test-unit-3.1.4' '+~amd64'
+    update_keywords '=dev-ruby/minitest-5.8.3' '+~amd64'
+    update_keywords '=dev-ruby/test-unit-3.1.5-r1' '+~amd64'
     update_keywords '=virtual/rubygems-11' '+~amd64'
     update_keywords '=dev-ruby/json-1.8.3' '+~amd64'
+    update_keywords '=dev-ruby/net-telnet-0.1.1-r1' '+~amd64'
+    update_keywords '=dev-ruby/did_you_mean-1.0.0' '+~amd64'
 }
 
 #
