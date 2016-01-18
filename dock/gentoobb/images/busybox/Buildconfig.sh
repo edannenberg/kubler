de362dc86d048c7f6c1d8a23c4ed76418919caf2
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2016-01-18 01:24:34 +0100

    Build busybox with musl instead of uclibc

--- dock/gentoobb/images/busybox/Buildconfig.sh
+++ dock/gentoobb/images/busybox/Buildconfig.sh
@@ -2,13 +2,14 @@
 # build config
 #
 PACKAGES="sys-apps/busybox"
-EMERGE_BIN="emerge-x86_64-pc-linux-uclibc"
+EMERGE_BIN="emerge-x86_64-pc-linux-musl"
 
 #
 # this method runs in the bb builder container just before starting the build of the rootfs
 # 
 configure_rootfs_build()
 {
+    export CHOST=x86_64-pc-linux-musl
     echo "sys-apps/busybox make-symlinks static" > /usr/x86_64-pc-linux-musl/etc/portage/package.use/busybox
 }
 
