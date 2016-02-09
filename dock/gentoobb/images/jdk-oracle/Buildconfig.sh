e04eb924d66ef91b94782f0816b83e577de0e9cd
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2016-02-09 21:58:22 +0100

    Update oracle java to 8u72. Add jce and use new download helper

--- dock/gentoobb/images/jdk-oracle/Buildconfig.sh
+++ dock/gentoobb/images/jdk-oracle/Buildconfig.sh
@@ -3,25 +3,24 @@
 #
 PACKAGES="dev-java/oracle-jdk-bin"
 
+
+
 #
 # this method runs in the bb builder container just before starting the build of the rootfs
 #
 configure_rootfs_build()
 {
     # download oracle jdk bin
-    JDK_URL=http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jdk-8u51-linux-x64.tar.gz
+    JDK_URL=http://download.oracle.com/otn-pub/java/jdk/8u72-b15/jdk-8u72-linux-x64.tar.gz
     #JDK_TAR=$(emerge -pf oracle-jdk-bin 2>&1 >/dev/null | grep -m1 "jre-[0-9a-z]*-linux-x64\.tar\.gz")
     regex="(jdk-[0-9a-z]*-linux-x64\.tar\.gz)"
-    if [[ ${JDK_URL} =~ $regex ]]; then
-        JDK_TAR="${BASH_REMATCH[1]}"
-    fi
-    if [ -n ${JDK_TAR} ] && [ ! -f /distfiles/${JDK_TAR} ]; then
-        wget --no-cookies --no-check-certificate \
-            --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
-            -P /distfiles \
-            "${JDK_URL}"
-    fi
-    update_use 'dev-java/oracle-jdk-bin' '-awt -fontconfig'
+    [[ ${JDK_URL} =~ $regex ]] && JDK_TAR="${BASH_REMATCH[1]}"
+    [ -n ${JDK_TAR} ] && [ ! -f /distfiles/${JDK_TAR} ] && download_from_oracle "${JDK_URL}"
+
+    JCE_URL=http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
+    [ ! -f /distfiles/${POLICY_URL} ] && download_from_oracle "${JCE_URL}"
+
+    update_use 'dev-java/oracle-jdk-bin' '+headless-awt +jce -fontconfig'
     # skip python and iced-tea
     provide_package dev-lang/python dev-java/icedtea-bin
 }
--- dock/gentoobb/images/jre-oracle/Buildconfig.sh
+++ dock/gentoobb/images/jre-oracle/Buildconfig.sh
@@ -9,19 +9,16 @@ PACKAGES="dev-java/oracle-jre-bin"
 configure_rootfs_build()
 {
     # download oracle jre bin
-    JRE_URL=http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jre-8u51-linux-x64.tar.gz
+    JRE_URL=http://download.oracle.com/otn-pub/java/jdk/8u72-b15/jre-8u72-linux-x64.tar.gz
     #JRE_TAR=$(emerge -pf oracle-jre-bin 2>&1 >/dev/null | grep -m1 "jre-[0-9a-z]*-linux-x64\.tar\.gz")
     regex="(jre-[0-9a-z]*-linux-x64\.tar\.gz)"
-    if [[ ${JRE_URL} =~ $regex ]]; then
-        JRE_TAR="${BASH_REMATCH[1]}"
-    fi
-    if [ -n ${JRE_TAR} ] && [ ! -f /distfiles/${JRE_TAR} ]; then
-        wget --no-cookies --no-check-certificate \
-            --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
-            -P /distfiles \
-            "${JRE_URL}"
-    fi
-    update_use 'dev-java/oracle-jre-bin' '-awt -fontconfig'
+    [[ ${JRE_URL} =~ $regex ]] && JRE_TAR="${BASH_REMATCH[1]}"
+    [ -n ${JRE_TAR} ] && [ ! -f /distfiles/${JRE_TAR} ] && download_from_oracle "${JRE_URL}"
+
+    JCE_URL=http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
+    [ ! -f /distfiles/${POLICY_URL} ] && download_from_oracle "${JCE_URL}"
+
+    update_use 'dev-java/oracle-jre-bin' '+headless-awt +jce -fontconfig'
     # skip python and iced-tea
     provide_package dev-lang/python dev-java/icedtea-bin
 }
