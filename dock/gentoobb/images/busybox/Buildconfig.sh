f11e4e3562161e32014ff18c59ae84451be6ff13
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2016-03-22 03:00:29 +0100

    Add missing busybox unzip regression patch to musl overlay. #30

--- dock/gentoobb/images/busybox/Buildconfig.sh
+++ dock/gentoobb/images/busybox/Buildconfig.sh
@@ -3,6 +3,8 @@
 #
 PACKAGES="sys-apps/busybox"
 EMERGE_BIN="emerge-${BOB_BUSYBOX_CHOST}"
+# fix digest of busybox ebuild in musl overlay after patching
+EMERGE_OPT="--digest"
 
 #
 # this method runs in the bb builder container just before starting the build of the rootfs
@@ -11,6 +13,23 @@ configure_rootfs_build()
 {
     export CHOST=${BOB_BUSYBOX_CHOST}
     echo "sys-apps/busybox make-symlinks static" > /usr/${BOB_BUSYBOX_CHOST}/etc/portage/package.use/busybox
+    # add missing busybox unzip regression patch to musl overlay and fix patch order in ebuild
+    # https://bugs.gentoo.org/show_bug.cgi?id=567340
+    cp /usr/portage/sys-apps/busybox/files/busybox-1.24.1-unzip-regression.patch /var/lib/layman/musl/sys-apps/busybox/files/
+    cat << 'EOP' | patch -p1 --ignore-whitespace /var/lib/layman/musl/sys-apps/busybox/busybox-1.24.1-r99.ebuild
+--- var/lib/layman/musl/sys-apps/busybox/busybox-1.24.1-r99.ebuild 2016-03-21 03:52:08.000000000 +0000
++++ usr/portage/sys-apps/busybox/busybox-1.24.1.ebuild 2015-12-31 05:51:48.000000000 +0000
+@@ -67,7 +67,8 @@
+
+    # patches go here!
+    epatch "${FILESDIR}"/${PN}-1.19.0-bb.patch
+-   epatch "${FILESDIR}"/${P}-*.patch
++   epatch "${FILESDIR}"/busybox-1.24.1-unzip.patch
++   epatch "${FILESDIR}"/busybox-1.24.1-unzip-regression.patch
+    cp "${FILESDIR}"/ginit.c init/ || die
+
+    # flag cleanup
+EOP
 }
 
 #
