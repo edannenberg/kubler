15af938f1608002b06029469fc76ec4c42dc0945
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2016-05-23 00:06:28 +0200

    Bump influxdb version to 0.13.0 and fix the build

--- dock/gentoobb/images/influxdb/Buildconfig.sh
+++ dock/gentoobb/images/influxdb/Buildconfig.sh
@@ -2,30 +2,26 @@
 # build config
 #
 PACKAGES=""
-INFLUXDB_VERSION="0.10.3"
+INFLUXDB_VERSION="0.13.0"
 
 configure_bob()
 {
     emerge -v go mercurial
-    export DISTRIBUTION_DIR=/go/src/github.com/influxdb/influxdb
+    export DISTRIBUTION_DIR=/go/src/github.com/influxdata/influxdb
     mkdir -p ${DISTRIBUTION_DIR}
     export GOPATH=/go
     git clone https://github.com/influxdb/influxdb.git ${DISTRIBUTION_DIR}
     cd ${DISTRIBUTION_DIR}
     git checkout tags/v${INFLUXDB_VERSION}
-    cd ../
-    echo "fetching deps.."
-    # occasionally go get will fail on first run, lets retry up to 5 times before giving up
-    for i in {1..5}; 
-        do go get ./... && break || (echo "retry fetch.." && sleep 5); done
     echo "building influxdb.."
-    go build ./...
-    go install ./...
+    # occasionally github clone rate limits fail the build, lets retry up to 5 times before giving up
+    for i in {1..5}; 
+        do ./build.py && break || (echo "retrying build in 5s.." && sleep 5); done
     echo "done."
 
     mkdir -p ${EMERGE_ROOT}/{bin,etc,var/opt/influxdb}
     cp "${DISTRIBUTION_DIR}/etc/config.sample.toml" "${EMERGE_ROOT}/etc/influxdb.conf"
-    cp /go/bin/* ${EMERGE_ROOT}/bin
+    cp ${DISTRIBUTION_DIR}/build/* "${EMERGE_ROOT}/bin"
 }
 
 #
