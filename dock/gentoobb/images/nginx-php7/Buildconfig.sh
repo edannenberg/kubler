058e029e9ad0e408c5bbb318fde971c4a4d4dda4
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2016-06-19 22:26:03 +0200

    Add php7 image

---
+++ dock/gentoobb/images/nginx-php7/Buildconfig.sh
@@ -0,0 +1,108 @@
+#
+# build config
+#
+PHP_SLOT="7.0"
+PHP_TARGET="php${PHP_SLOT/\./-}"
+PACKAGES="dev-lang/php dev-php/xdebug dev-php/pecl-apcu dev-libs/libmemcached media-gfx/imagemagick"
+#PACKAGES="dev-lang/php dev-php/pecl-memcached dev-php/pecl-redis pecl-imagick"
+PHP_TIMEZONE="${BOB_TIMEZONE:-UTC}"
+ADMINER_VERSION="4.2.5"
+#ICONV_FROM=gentoobb/glibc
+
+configure_bob()
+{
+    echo "PHP_TARGETS=\"${PHP_TARGET}\"" >> /etc/portage/make.conf
+    echo 'PHP_INI_VERSION="production"' >> /etc/portage/make.conf
+
+    update_keywords 'app-eselect/eselect-php' '+~amd64'
+    update_keywords 'dev-lang/php' '+~amd64'
+
+    update_use 'dev-lang/php' '+bcmath' '+calendar' '+curl' '+fpm' '+gd' '+mhash' \
+               '+mysql' '+mysqli' '+pcntl' '+pdo' '+soap' '+sockets' '+xmlreader' '+xmlrpc' '+xmlwriter' '+xpm' '+xslt' '+zip'
+    update_use 'app-eselect/eselect-php' '+fpm'
+    update_use 'media-gfx/imagemagick' '-openmp'
+    emerge php git libmemcached imagemagick
+}
+
+#
+# this method runs in the bb builder container just before starting the build of the rootfs
+#
+configure_rootfs_build()
+{
+    update_keywords 'dev-php/xdebug' '+~amd64'
+    update_keywords 'dev-php/xdebug-client' '+~amd64'
+    update_keywords 'dev-php/pecl-apcu' '+~amd64'
+
+    update_use 'dev-php/pecl-apcu' '+mmap'
+
+    # skip bash, perl, autogen. pulled in as dep since php 5.5.22
+    provide_package app-shells/bash dev-lang/perl sys-devel/autogen
+}
+
+#
+# this method runs in the bb builder container just before tar'ing the rootfs
+#
+finish_rootfs_build()
+{
+    # php memcached support
+    git clone https://github.com/php-memcached-dev/php-memcached.git
+    cd php-memcached/
+    git checkout php7
+    phpize
+    # our libtool is too new, regen some stuff with current version
+    aclocal; libtoolize --force; autoheader; autoconf
+    ./configure --disable-memcached-sasl
+    make
+    cp modules/* /emerge-root/usr/lib64/php7.0/lib/extensions/no-debug-zts-20151012/
+    echo 'extension=/usr/lib64/php7.0/lib/extensions/no-debug-zts-20151012/memcached.so' > /emerge-root/etc/php/fpm-php7.0/ext-active/memcached.ini
+
+    # php redis support
+    cd ..
+    git clone https://github.com/phpredis/phpredis.git
+    cd phpredis/
+    git checkout php7
+    phpize
+    # our libtool is too new, regen some stuff with current version
+    aclocal; libtoolize --force; autoheader; autoconf
+    ./configure
+    make
+    cp modules/* /emerge-root/usr/lib64/php7.0/lib/extensions/no-debug-zts-20151012/
+    echo 'extension=/usr/lib64/php7.0/lib/extensions/no-debug-zts-20151012/redis.so' > /emerge-root/etc/php/fpm-php7.0/ext-active/redis.ini
+
+    # php imagick support
+    cd ..
+    wget https://pecl.php.net/get/imagick-3.4.1.tgz
+    tar xvzf imagick-3.4.1.tgz
+    cd imagick-3.4.1/
+    phpize
+    # our libtool is too new, regen some stuff with current version
+    aclocal; libtoolize --force; autoheader; autoconf
+    ./configure
+    make
+    cp modules/* /emerge-root/usr/lib64/php7.0/lib/extensions/no-debug-zts-20151012/
+    echo 'extension=/usr/lib64/php7.0/lib/extensions/no-debug-zts-20151012/imagick.so' > /emerge-root/etc/php/fpm-php7.0/ext-active/imagick.ini
+
+    # set php iconv default to UTF-8, if you need full iconv functionality set ICONV_FROM=gentoobb/glibc above
+    sed -i 's/^;iconv.input_encoding = ISO-8859-1/iconv.input_encoding = UTF-8/g' $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/php.ini
+    sed -i 's/^;iconv.internal_encoding = ISO-8859-1/iconv.internal_encoding = UTF-8/g' $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/php.ini
+    sed -i 's/^;iconv.output_encoding = ISO-8859-1/iconv.output_encoding = UTF-8/g' $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/php.ini
+    # set php time zone
+    sed -i "s@^;date.timezone =@date.timezone = $PHP_TIMEZONE@g" $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/php.ini
+    # use above changes also for php cli config
+    cp $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/php.ini $EMERGE_ROOT/etc/php/cli-php${PHP_SLOT}/php.ini
+    # disable xdebug
+    rm $EMERGE_ROOT/etc/php/fpm-php${PHP_SLOT}/ext-active/xdebug.ini
+    rm $EMERGE_ROOT/etc/php/cli-php${PHP_SLOT}/ext-active/xdebug.ini
+    # required by null-mailer
+    copy_gcc_libs
+    chmod 0640 $EMERGE_ROOT/etc/nullmailer/remotes
+    # apparently a bug with nullmailer? links to non existing gnutls lib
+    ln -r -s $EMERGE_ROOT/usr/lib64/libgnutls.so.28 $EMERGE_ROOT/usr/lib64/libgnutls.so.26
+    # required by imagick
+    find /usr/lib64/gcc/x86_64-pc-linux-gnu -name libgomp.so.* -exec cp {} $EMERGE_ROOT/usr/lib64/ \;
+    # prepare adminer / phpinfo micro sites
+    mkdir -p $EMERGE_ROOT/var/www/{adminer,phpinfo}
+    wget -O $EMERGE_ROOT/var/www/adminer/adminer.php https://www.adminer.org/static/download/${ADMINER_VERSION}/adminer-${ADMINER_VERSION}-en.php
+    wget -O $EMERGE_ROOT/var/www/adminer/adminer.css https://raw.github.com/vrana/adminer/master/designs/bueltge/adminer.css
+    echo "<?php phpinfo(); ?>" > $EMERGE_ROOT/var/www/phpinfo/phpinfo.php
+}
---
+++ dock/gentoobb/images/nginx-php7/etc/nginx/link_php_microsites.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$XDEBUG_ENABLED" == 'true' ]; then
+    if [ ! -f /etc/php/fpm-php7.0/ext-active/xdebug.ini ] && [ -f /etc/php/fpm-php7.0/ext/xdebug.ini ]; then
+        ln -s /etc/php/fpm-php7.0/ext/xdebug.ini /etc/php/fpm-php7.0/ext-active
+        ln -s /etc/php/cli-php7.0/ext/xdebug.ini /etc/php/cli-php7.0/ext-active
+    fi
+fi
+
+if [ ! -z $NG_TMPL_ADMINER_URL ]; then
+    if [ ! -f /etc/nginx/sites-enabled/adminer.conf ] && [ -f /etc/nginx/sites-all/adminer.conf ]; then
+        ln -s /etc/nginx/sites-all/adminer.conf /etc/nginx/sites-enabled
+    fi
+fi
+
+if [ ! -z $NG_TMPL_PHPINFO_URL ]; then
+    if [ ! -f /etc/nginx/sites-enabled/phpinfo.conf ] && [ -f /etc/nginx/sites-all/phpinfo.conf ]; then
+        ln -s /etc/nginx/sites-all/phpinfo.conf /etc/nginx/sites-enabled
+    fi
+fi
