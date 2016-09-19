afbaaadd9b5353357a0f9f1ba10a60065f9d0165
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2016-09-19 03:37:56 +0200

    Use nginx from mva overlay

--- dock/gentoobb/images/nginx/Buildconfig.sh
+++ dock/gentoobb/images/nginx/Buildconfig.sh
@@ -1,7 +1,13 @@
 #
 # build config
 #
-PACKAGES="www-servers/nginx"
+PACKAGES="www-servers/nginx::mva"
+
+configure_bob()
+{
+    # add mva overlay which has nginx with pagespeed and other goodies
+    layman -a mva
+}
 
 #
 # this method runs in the bb builder container just before starting the build of the rootfs
@@ -9,8 +15,9 @@ PACKAGES="www-servers/nginx"
 configure_rootfs_build()
 {
     echo 'NGINX_MODULES_HTTP="access auth_basic autoindex charset fastcgi \
-             gzip gzip_static limit_req map proxy rewrite scgi ssi stub_status"' >> /etc/portage/make.conf
+             gzip gzip_static limit_req map proxy rewrite scgi ssi stub_status v2"' >> /etc/portage/make.conf
     echo 'NGINX_MODULES_MAIL=""' >> /etc/portage/make.conf
+    update_keywords 'www-servers/nginx' '+~amd64'
     update_use 'www-servers/nginx' '+http2'
     update_use 'dev-libs/libpcre' '-readline'
 }
@@ -21,4 +28,9 @@ configure_rootfs_build()
 finish_rootfs_build()
 {
     mkdir -p $EMERGE_ROOT/etc/nginx/conf.d
+    # required if pagespeed module is included
+    #copy_gcc_libs
+    # remove overlay
+    unset ROOT
+    layman -d mva
 }
