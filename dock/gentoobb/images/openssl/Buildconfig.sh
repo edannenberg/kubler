4eb57597b40dc581e3ef55fd9e3d90028f0a704f
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2016-09-19 03:13:59 +0200

    Enable ECDH for openssl

--- dock/gentoobb/images/openssl/Buildconfig.sh
+++ dock/gentoobb/images/openssl/Buildconfig.sh
@@ -3,6 +3,15 @@
 #
 PACKAGES="dev-libs/openssl"
 
+configure_bob()
+{
+    # enable ECDH
+    emerge -C net-misc/openssh
+    update_use 'dev-libs/openssl' '-bindist'
+    unprovide_package 'dev-libs/openssl'
+    emerge dev-libs/openssl
+}
+
 #
 # this method runs in the bb builder container just before starting the build of the rootfs
 #
