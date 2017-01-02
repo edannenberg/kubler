6bcd9929beb4f28f1366ab5ce4817c0cadb0587b
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2017-01-02 10:08:35 +0100

    Update to ruby 2.4

--- dock/gentoobb/images/ruby-gcc/Buildconfig.sh
+++ dock/gentoobb/images/ruby-gcc/Buildconfig.sh
@@ -9,20 +9,22 @@ KEEP_HEADERS=true
 #
 configure_rootfs_build()
 {
-    echo 'RUBY_TARGETS="ruby23"' >> /etc/portage/make.conf
+    echo 'RUBY_TARGETS="ruby24"' >> /etc/portage/make.conf
     update_keywords 'dev-lang/ruby' '+~amd64'
-    update_keywords '=app-eselect/eselect-ruby-20151229' '+~amd64'
+    update_keywords '=app-eselect/eselect-ruby-20161226' '+~amd64'
     update_keywords '=dev-ruby/racc-1.4.14' '+~amd64'
-    update_keywords '=dev-ruby/rdoc-4.2.2' '+~amd64'
-    update_keywords '=dev-ruby/rubygems-2.6.6' '+~amd64'
-    update_keywords '=dev-ruby/rake-11.2.2-r1' '+~amd64'
-    update_keywords '=dev-ruby/power_assert-0.2.6' '+~amd64'
-    update_keywords '=dev-ruby/minitest-5.9.1' '+~amd64'
-    update_keywords '=dev-ruby/test-unit-3.1.5-r1' '+~amd64'
-    update_keywords '=virtual/rubygems-11' '+~amd64'
-    update_keywords '=dev-ruby/json-1.8.3' '+~amd64'
+    update_keywords '=dev-ruby/rdoc-5.0.0-r2' '+~amd64'
+    update_keywords '=dev-ruby/rubygems-2.6.8' '+~amd64'
+    update_keywords '=dev-ruby/rake-12.0.0' '+~amd64'
+    update_keywords '=dev-ruby/power_assert-0.4.1' '+~amd64'
+    update_keywords '=dev-ruby/minitest-5.10.1' '+~amd64'
+    update_keywords '=dev-ruby/test-unit-3.2.3' '+~amd64'
+    update_keywords '=virtual/rubygems-12' '+~amd64'
+    update_keywords '=dev-ruby/json-2.0.2' '+~amd64'
     update_keywords '=dev-ruby/net-telnet-0.1.1-r1' '+~amd64'
-    update_keywords '=dev-ruby/did_you_mean-1.0.0' '+~amd64'
+    update_keywords '=dev-ruby/did_you_mean-1.1.0' '+~amd64'
+    update_keywords '=dev-ruby/xmlrpc-0.2.1' '+~amd64'
+    update_keywords '=dev-ruby/kpeg-1.1.0' '+~amd64'
 }
 
 #
