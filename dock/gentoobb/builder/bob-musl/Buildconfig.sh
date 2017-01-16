14358a100a1a5baff6eaa5c9c265a09f87b64a7f
Azimut <you@example.com>
2017-01-16 07:07:53 -0300

    crossdev: add log to stdout on error

--- dock/gentoobb/builder/bob-musl/Buildconfig.sh
+++ dock/gentoobb/builder/bob-musl/Buildconfig.sh
@@ -15,7 +15,7 @@ priority=9999
 masters = gentoo
 auto-sync = no" > /etc/portage/repos.conf/crossdev.conf
 
-    crossdev -S --init-target --target ${CROSSDEV_MUSL}
+    crossdev --show-fail-log -S --init-target --target ${CROSSDEV_MUSL}
     mkdir -p /usr/${CROSSDEV_MUSL}/etc/portage/package.{mask,unmask,use,keywords} /usr/${CROSSDEV_MUSL}/tmp/
     rm /usr/${CROSSDEV_MUSL}/etc/portage/make.profile
     ln -s /usr/portage/profiles/hardened/linux/musl/amd64 /usr/${CROSSDEV_MUSL}/etc/portage/make.profile
@@ -48,7 +48,7 @@ PKGDIR="/packages/${CHOST}"' /usr/${CROSSDEV_MUSL}/etc/portage/make.conf
     # fix regression in latest toolchain.eclass - see https://bugs.gentoo.org/show_bug.cgi?id=548782
     sed -i 's/\.\/\${dir}\/\*\.la || die/\.\/\${dir}\/\*\.la/g' /usr/portage/eclass/toolchain.eclass
 
-    crossdev -S --target ${CROSSDEV_MUSL}
+    crossdev --show-fail-log -S --target ${CROSSDEV_MUSL}
 
     rm /etc/portage/package.use/gcc
 }
