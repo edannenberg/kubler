5509135c1dabc8cf79a4ca1ebc6cda42122c4123
Berney <berne.campbell@gmail.com>
2017-02-10 21:04:19 +1100

    Fix BOB_SKIP_BASELAYOUT, should be variable not literal

--- bob-core/build-root.sh
+++ bob-core/build-root.sh
@@ -346,7 +346,7 @@ if [ -n "$PACKAGES" ]; then
     init_docs ${REPO/\images\//}
     generate_doc_package_installed "${PACKAGES}"
 
-    if [ -z "BOB_SKIP_BASELAYOUT" ]; then
+    if [ -z "$BOB_SKIP_BASELAYOUT" ]; then
         "${EMERGE_BIN}" ${EMERGE_OPT} --binpkg-respect-use=y -v sys-apps/baselayout
     fi
     # install packages (defined via build.sh)
@@ -385,7 +385,7 @@ generate_documentation_footer
 unset ROOT
 
 # /run symlink
-if [ -z "BOB_SKIP_BASELAYOUT" ]; then
+if [ -z "$BOB_SKIP_BASELAYOUT" ]; then
     mkdir -p $EMERGE_ROOT/{run,var} && ln -s /run $EMERGE_ROOT/var/run
 fi
 
