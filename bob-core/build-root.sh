edannenberg/kubler

commit 4064e3622208eb7e32f37ea6a9643cf80d3a9b4a

Disable Portage binpkg features temp. when asking for package versions. #78
Also uses eix now to simplify pipes.

bob-core/build-root.sh
@@ -140,10 +140,14 @@ write_checkbox_line() {
 # 1: PACKAGES (i.e. "sys-apps/busybox dev-vcs/git")
 generate_package_installed() {
     local PACKAGES="${1}"
+    # disable binary package features temporarily to work around binpkg_multi_instance altering the version string
+    local current_emerge_opts="${EMERGE_DEFAULT_OPTS}"
+    export EMERGE_DEFAULT_OPTS=""
     # generate installed package list
     "${EMERGE_BIN}" ${EMERGE_OPT} --binpkg-respect-use=y -p ${PACKAGES[@]} | \
-    grep -Eow "\[.*\] (.*) to" | \
-    awk '{print $(NF-1)}' > ${PACKAGE_INSTALLED}
+        eix '-|*' --format '<markedversions:NAMEVERSION>' > ${PACKAGE_INSTALLED}
+    # enable binary package features again
+    export EMERGE_DEFAULT_OPTS="${current_emerge_opts}"
 }
 
 # Append DOC_PACKAGE_INSTALLED from last build to DOC_PACKAGE_PROVIDED, overwrite DOC_PACKAGE_INSTALLED with header for current build
@@ -167,10 +171,15 @@ init_docs() {
 # 1: PACKAGES (i.e. "shell/bash dev-vcs/git")
 generate_doc_package_installed() {
     local PACKAGES="${1}"
+    # disable binary package features temporarily to work around binpkg_multi_instance altering the version string
+    local current_emerge_opts="${EMERGE_DEFAULT_OPTS}"
+    export EMERGE_DEFAULT_OPTS=""
     # generate installed package list with use flags
     "${EMERGE_BIN}" ${EMERGE_OPT} --binpkg-respect-use=y -p ${PACKAGES[@]} | \
         perl -nle 'print "$1 | `$3`" if /\[.*\] (.*) to \/.*\/( USE=")?([a-z0-9\- (){}]*)?/' | \
         sed /^virtual/d | sort -u >> "${DOC_PACKAGE_INSTALLED}"
+    # enable binary package features again
+    export EMERGE_DEFAULT_OPTS="${current_emerge_opts}"
 }
 
 # Adds a package entry in $DOC_PACKAGE_INSTALLED to document non-Portage package installs.
@@ -218,9 +227,16 @@ unmask_package() {
 # 1: package atom (i.e. app-shells/bash)
 # n: more package atoms
 provide_package() {
+    # disable binary package features temporarily to work around binpkg_multi_instance altering the version string
+    local current_emerge_opts="${EMERGE_DEFAULT_OPTS}"
+    export EMERGE_DEFAULT_OPTS=""
     for P in ${@}; do
-        emerge -p "${P}" | grep "${P}" | grep -Eow "\[.*\] (.*) to" | awk '{print $(NF-1)}' >> /etc/portage/profile/package.provided
+        "${EMERGE_BIN}" ${EMERGE_OPT} --binpkg-respect-use=y -p "${P}" | \
+            eix '-|*' --format '<markedversions:NAMEVERSION>' | \
+            grep "${P}" >> /etc/portage/profile/package.provided
     done
+    # enable binary package features again
+    export EMERGE_DEFAULT_OPTS="${current_emerge_opts}"
 }
 
 # Mark package atom for reinstall.

dock/gentoobb/builder/bob-musl/build.sh
@@ -26,7 +26,9 @@ configure_bob() {
     update_use 'app-crypt/pinentry' '+ncurses'
     update_keywords 'app-portage/layman' '+~amd64'
     update_keywords 'dev-python/ssl-fetch' '+~amd64'
-    emerge dev-vcs/git app-portage/layman sys-devel/distcc
+    emerge dev-vcs/git app-portage/layman sys-devel/distcc app-misc/jq app-portage/eix
+    # init eix portage db
+    eix-update
     # setup layman
     sed -i 's/^check_official : Yes/check_official : No/g' /etc/layman/layman.cfg # no pesky prompts please
     layman -L

dock/gentoobb/builder/bob/build.sh
@@ -23,7 +23,9 @@ configure_bob() {
     update_use 'app-crypt/pinentry' '+ncurses'
     update_keywords 'app-portage/layman' '+~amd64'
     update_keywords 'dev-python/ssl-fetch' '+~amd64'
-    emerge sys-devel/crossdev dev-vcs/git app-portage/layman sys-devel/distcc
+    emerge sys-devel/crossdev dev-vcs/git app-portage/layman sys-devel/distcc app-misc/jq app-portage/eix
+    # init eix portage db
+    eix-update
     # setup layman
     sed -i 's/^check_official : Yes/check_official : No/g' /etc/layman/layman.cfg # no pesky prompts please
     layman -L

commit c6e5f932b083ce668336cc9f768e9ddff4d22fd1

Fix regression introduced by 4064e36222, eix exits with signal 1 when no packages are processed, failing the build

bob-core/build-root.sh
@@ -144,8 +144,11 @@ generate_package_installed() {
     local current_emerge_opts="${EMERGE_DEFAULT_OPTS}"
     export EMERGE_DEFAULT_OPTS=""
     # generate installed package list
+    set +e
     "${EMERGE_BIN}" ${EMERGE_OPT} --binpkg-respect-use=y -p ${PACKAGES[@]} | \
         eix '-|*' --format '<markedversions:NAMEVERSION>' > ${PACKAGE_INSTALLED}
+    [[ $? -gt 1 ]] && echo "Error generating package.installed" && exit $?
+    set -e
     # enable binary package features again
     export EMERGE_DEFAULT_OPTS="${current_emerge_opts}"
 }
@@ -175,9 +178,12 @@ generate_doc_package_installed() {
     local current_emerge_opts="${EMERGE_DEFAULT_OPTS}"
     export EMERGE_DEFAULT_OPTS=""
     # generate installed package list with use flags
+    set +e
     "${EMERGE_BIN}" ${EMERGE_OPT} --binpkg-respect-use=y -p ${PACKAGES[@]} | \
         perl -nle 'print "$1 | `$3`" if /\[.*\] (.*) to \/.*\/( USE=")?([a-z0-9\- (){}]*)?/' | \
         sed /^virtual/d | sort -u >> "${DOC_PACKAGE_INSTALLED}"
+    [[ $? -gt 1 ]] && echo "Error generating doc.package.installed" && exit $?
+    set -e
     # enable binary package features again
     export EMERGE_DEFAULT_OPTS="${current_emerge_opts}"
 }