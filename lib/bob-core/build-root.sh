3be1fb8581ae6bd90fa9d883c0c97ef5b0607a38
soredake <fdsfgs@krutt.org>
2017-03-27 04:58:01 +0300

    Add alternative overlay function to build-root.sh #112

--- lib/bob-core/build-root.sh
+++ lib/bob-core/build-root.sh
@@ -368,6 +368,33 @@ function add_layman_overlay() {
     layman -a ${@}
 }
 
+# Add Gentoo overlay to repos.conf/ and sync it
+# Example usage: add_overlay musl https://anongit.gentoo.org/git/proj/musl.git
+#
+# Arguments:
+#
+# 1: repo_id - reference used in repos.conf
+# 2: repo_url
+# 3: repo_mode - optional, default: git
+# 4: repo_priority - optional, default: 50
+add_overlay() {
+    local repo_id repo_url repo_mode repo_priority repo_path
+    repo_id="$1"
+    repo_url="$2"
+    repo_mode="${3:-git}"
+    repo_priority="${4:-50}"
+    repo_path='/var/lib/repos'
+    [ ! -d "${repo_path}" ] && mkdir -p "${repo_path}"
+    tee /etc/portage/repos.conf/"${repo_id}".conf >/dev/null <<END
+["${repo_id}"]
+priority = "${repo_priority}"
+location = "${repo_path}/${repo_id}"
+sync-type = "${repo_mode}"
+sync-uri = "${repo_url}"
+END
+    emaint sync -r "${repo_id}"
+}
+
 function install_oci_deps() {
     local acserver_path
     export GOPATH='/go'
