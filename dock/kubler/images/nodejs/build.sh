f6906725676fb9c7ebcfe6e2f294a60fff8d52c0
azimut <azimut.github@protonmail.com>
2017-03-28 09:23:11 -0300

    add zlib dependency for nodejs

--- dock/kubler/images/nodejs/build.sh
+++ dock/kubler/images/nodejs/build.sh
@@ -1,13 +1,13 @@
 #
 # Kubler phase 1 config, pick installed packages and/or customize the build
 #
-_packages="net-libs/http-parser dev-libs/libuv dev-libs/icu net-libs/nodejs"
+_packages="sys-libs/zlib net-libs/http-parser dev-libs/libuv dev-libs/icu net-libs/nodejs"
 
 configure_bob()
 {
     update_use net-libs/nodejs +icu
     # build binary packages first to avoid pulling in python in the next phase
-    emerge net-libs/http-parser dev-libs/libuv dev-libs/icu net-libs/nodejs
+    emerge sys-libs/zlib net-libs/http-parser dev-libs/libuv dev-libs/icu net-libs/nodejs
 }
 
 #
