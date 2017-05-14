edannenberg/kubler

commit 84a5fb088c49f97b72ef2723496dbf16f84694a7

Fix wrong builder_commit_id when mixing build containers #87

lib/engine/docker.sh
@@ -149,7 +149,7 @@ function build_image() {
         if [[ ! -z "${BUILDER}" ]]; then
             builder_commit_id="${BUILDER##*/}-${current_image}"
         elif [[ "${image_type}" == "${_IMAGE_PATH}" ]]; then
-            builder_commit_id="${DEFAULT_BUILDER##*/}-${current_image}"
+            builder_commit_id="${builder_id##*/}-${current_image}"
         fi
 
         if [[ "${image_type}" == "${_BUILDER_PATH}" ]]; then

commit 17e12f95166bc9de956e9769c8011da12ab1e47f

Fix builder_commit_id handling, only use base id from parent #87
84a5fb0 introduced a regression that caused build container image
names to include the whole parent build container id, i.e for
kubler/s6:

expected : kubler/bob-s6
actual   : kubler/bob-glibc-s6

lib/engine/docker.sh
@@ -149,7 +149,10 @@ function build_image() {
         if [[ ! -z "${BUILDER}" ]]; then
             builder_commit_id="${BUILDER##*/}-${current_image}"
         elif [[ "${image_type}" == "${_IMAGE_PATH}" ]]; then
-            builder_commit_id="${builder_id##*/}-${current_image}"
+            local builder_image_id image_parent_id
+            builder_image_id="${builder_id##*/}"
+            image_parent_id="${IMAGE_PARENT##*/}"
+            builder_commit_id="${builder_image_id%-${image_parent_id}}-${current_image}"
         fi
 
         if [[ "${image_type}" == "${_BUILDER_PATH}" ]]; then