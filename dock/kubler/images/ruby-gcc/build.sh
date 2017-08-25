6eed1f1f7c7bfc79ac9122cda19efd0d4532f504
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2017-08-25 18:50:55 +0200

    [ruby] Update deps

--- dock/kubler/images/ruby-gcc/build.sh
+++ dock/kubler/images/ruby-gcc/build.sh
@@ -17,7 +17,7 @@ configure_rootfs_build()
     update_keywords 'dev-lang/ruby' '+~amd64'
     update_keywords '=dev-ruby/racc-1.4.14' '+~amd64'
     update_keywords '=dev-ruby/rdoc-5.1.0' '+~amd64'
-    update_keywords '=dev-ruby/rubygems-2.6.8' '+~amd64'
+    update_keywords '=dev-ruby/rubygems-2.6.12' '+~amd64'
     update_keywords '=dev-ruby/rake-12.0.0' '+~amd64'
     update_keywords '=dev-ruby/power_assert-0.4.1' '+~amd64'
     update_keywords '=dev-ruby/minitest-5.10.3' '+~amd64'
