1dc9be54b62cf5b236bffa74b009fdae4f53c1b4
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2017-10-20 20:09:22 +0200

    Update builder template

--- lib/template/docker/builder/build.sh
+++ lib/template/docker/builder/build.sh
@@ -20,6 +20,10 @@ configure_bob() {
     env-update
     source /etc/profile
     # install default packages
+    # when using overlay1 docker storage the created hard link will trigger an error during openssh uninstall
+    [[ -f /usr/"${_LIB}"/misc/ssh-keysign ]] && rm /usr/"${_LIB}"/misc/ssh-keysign
+    emerge -C net-misc/openssh
+    update_use 'dev-libs/openssl' -bindist
     update_use 'dev-vcs/git' '-perl'
     update_use 'app-crypt/pinentry' '+ncurses'
     update_keywords 'app-portage/layman' '+~amd64'
