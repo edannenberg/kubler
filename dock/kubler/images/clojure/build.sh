6bb4721edbc7b3e40f10b95ad43ecba2783e59b5
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2017-11-29 06:40:57 +0100

    [clojure] Bump version to 1.9.0-RC1

--- dock/kubler/images/clojure/build.sh
+++ dock/kubler/images/clojure/build.sh
@@ -2,7 +2,7 @@
 # Kubler phase 1 config, pick installed packages and/or customize the build
 #
 _packages=""
-_clojure_version=1.9.0-beta3
+_clojure_version=1.9.0-RC1
 
 #
 # This hook is called just before starting the build of the root fs
