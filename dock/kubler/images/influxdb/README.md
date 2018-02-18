68edd4bbce04d3ee0f19bf900da6e9b40c651296
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2018-02-18 22:11:05 +0100

    [influxdb] Refactor to use kubler-overlay package

--- dock/kubler/images/influxdb/README.md
+++ dock/kubler/images/influxdb/README.md
@@ -4,7 +4,7 @@ Run this [InfluxDB][] image with:
 
     $ mkdir /var/lib/influxdb
     $ docker run -d --name influxdb-0 \
-       -v /var/lib/influxdb:/var/opt/influxdb \
+       -v /var/lib/influxdb:/var/lib/influxdb \
        -p 8083:8083 -p 8086:8086 -p 8090:8090 -p 8099:8099 kubler/influxdb
 
 Web Interface (obsolete since 1.1.0):
--- dock/kubler/images/influxdb/build.sh
+++ dock/kubler/images/influxdb/build.sh
@@ -1,36 +1,14 @@
 #
 # Kubler phase 1 config, pick installed packages and/or customize the build
 #
-_packages=""
-_influxdb_version="1.4.2"
-
-configure_bob()
-{
-    emerge -v dev-vcs/mercurial
-    export DISTRIBUTION_DIR=/go/src/github.com/influxdata/influxdb
-    mkdir -p "${DISTRIBUTION_DIR}"
-    export GOPATH=/go
-    git clone https://github.com/influxdb/influxdb.git "${DISTRIBUTION_DIR}"
-    cd "${DISTRIBUTION_DIR}"
-    git checkout "tags/v${_influxdb_version}"
-    echo "building influxdb.."
-    # occasionally github clone rate limits fail the build, lets retry up to 5 times before giving up
-    local i
-    for i in {1..5}; 
-        do ./build.py && break || { echo "retrying build in 5s.."; sleep 5; }; done
-    echo "done."
-
-    mkdir -p "${_EMERGE_ROOT}"/{bin,etc,var/opt/influxdb}
-    cp "${DISTRIBUTION_DIR}"/etc/config.sample.toml "${_EMERGE_ROOT}"/etc/influxdb.conf
-    cp "${DISTRIBUTION_DIR}"/build/* "${_EMERGE_ROOT}"/bin
-}
+_packages="dev-db/influxdb"
 
 #
 # This hook is called just before starting the build of the root fs
 #
 configure_rootfs_build()
 {
-    init_docs "kubler/influxdb"
+    :
 }
 
 #
@@ -38,5 +16,5 @@ configure_rootfs_build()
 #
 finish_rootfs_build()
 {
-    log_as_installed "manual install" "influxdb-${_influxdb_version}" "https://github.com/influxdb/influxdb/"
+    :
 }
