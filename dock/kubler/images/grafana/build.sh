5ab18cb68ebfbc06ffa419c7a82731b0377c8809
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2018-02-18 22:10:18 +0100

    [grafana] Refactor to use kubler-overlay package

--- dock/kubler/images/grafana/build.sh
+++ dock/kubler/images/grafana/build.sh
@@ -1,39 +1,14 @@
 #
 # Kubler phase 1 config, pick installed packages and/or customize the build
 #
-_packages=""
-_grafana_version="4.6.3"
-
-configure_bob()
-{
-    emerge -v net-libs/nodejs
-    export DISTRIBUTION_DIR=/go/src/github.com/grafana/grafana
-    mkdir -p "${DISTRIBUTION_DIR}"
-    export GOPATH=/go
-    export PATH="${PATH}:/go/bin"
-    git clone https://github.com/grafana/grafana.git "${DISTRIBUTION_DIR}"
-    cd "${DISTRIBUTION_DIR}"
-    git checkout "tags/v${_grafana_version}"
-    echo "building grafana.."
-    go run build.go build
-
-    npm install -g yarn
-    yarn install --pure-lockfile
-    npm run build
-
-    mkdir -p "${_EMERGE_ROOT}"/opt/grafana/{bin,conf,data}
-    cp -rp "${DISTRIBUTION_DIR}"/public "${_EMERGE_ROOT}"/opt/grafana/
-    cp "${DISTRIBUTION_DIR}"/conf/defaults.ini "${_EMERGE_ROOT}"/opt/grafana/conf/
-    cp "${DISTRIBUTION_DIR}"/conf/sample.ini "${_EMERGE_ROOT}"/opt/grafana/conf/custom.ini
-    cp "${DISTRIBUTION_DIR}"/bin/* "${_EMERGE_ROOT}"/opt/grafana/bin
-}
+_packages="www-apps/grafana"
 
 #
 # This hook is called just before starting the build of the root fs
 #
 configure_rootfs_build()
 {
-    init_docs "kubler/grafana"
+    update_use 'sys-apps/yarn' '+~amd64'
 }
 
 #
@@ -41,5 +16,6 @@ configure_rootfs_build()
 #
 finish_rootfs_build()
 {
-    log_as_installed "manual install" "grafana-${_grafana_version}" "https://github.com/grafana/grafana/"
+    mkdir -p "${_EMERGE_ROOT}"/opt/grafana
+    ln -sr "${_EMERGE_ROOT}"/usr/share/grafana/{conf,public} "${_EMERGE_ROOT}"/opt/grafana/
 }
