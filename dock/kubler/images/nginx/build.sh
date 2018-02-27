f98fc3786ccdf54808284d9d09c80298877f7e50
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2018-02-27 01:00:44 +0100

    [nginx] Add realip

--- dock/kubler/images/nginx/build.sh
+++ dock/kubler/images/nginx/build.sh
@@ -18,7 +18,7 @@ configure_bob()
 configure_rootfs_build()
 {
     echo 'NGINX_MODULES_HTTP="access auth_basic autoindex charset fastcgi \
-             gzip gzip_static limit_req map proxy rewrite scgi ssi stub_status v2"' >> /etc/portage/make.conf
+             gzip gzip_static limit_req map proxy realip rewrite scgi ssi stub_status v2"' >> /etc/portage/make.conf
     echo 'NGINX_MODULES_MAIL=""' >> /etc/portage/make.conf
     update_keywords 'www-servers/nginx' '+~amd64'
     update_use 'www-servers/nginx' '+http2'
