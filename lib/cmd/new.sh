422b2b0b034c3bdd4f6423543618f4cd19c5b1ba
Monthadar Al-Jaberi <monthadar.al-jaberi@infinera.com>
2018-03-04 23:13:41 +0100

    Verify new namespace or image names are lower case in `new` cmd #144
    As Docker only allows lower case image names anything else will
    fail the build.

--- lib/cmd/new.sh
+++ lib/cmd/new.sh
@@ -281,6 +281,9 @@ function main() {
     target_namespace="${target_id%%/*}"
     target_image_name="${target_id##*/}"
 
+    [[ "${target_id}" =~ [A-Z] ]] \
+            && die "Invalid ${_arg_template_type} name '${target_id}', should be lower case only."
+
     if [[ "${_arg_template_type}" != 'namespace' ]]; then
         [[ "${target_id}" != *"/"* ]] && die "\"${target_id}\" should have format <namespace>/<image_name>"
         [[ "${_NAMESPACE_TYPE}" == 'none' ]] \
