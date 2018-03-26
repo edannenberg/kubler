2d2d33551435690ab850151e6b667d6eeea33a15
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2018-03-27 01:25:19 +0200

    [webhook] Refactor to use kubler-overlay package

--- dock/kubler/images/webhook/build.sh
+++ dock/kubler/images/webhook/build.sh
@@ -1,25 +1,8 @@
 #
 # Kubler phase 1 config, pick installed packages and/or customize the build
 #
-_packages="dev-vcs/git"
+_packages="dev-vcs/git dev-vcs/webhook"
 _webhook_version="2.6.8"
-
-configure_bob()
-{
-    export GOPATH=/go
-    export DISTRIBUTION_DIR="${GOPATH}/src/github.com/adnanh/webhook"
-    mkdir -p "${DISTRIBUTION_DIR}"
-    git clone https://github.com/adnanh/webhook.git "${DISTRIBUTION_DIR}"
-    cd "${DISTRIBUTION_DIR}"
-    git checkout "tags/${_webhook_version}"
-    echo "building webhook.."
-    # occasionally github clone rate limits fail the build, lets retry up to 5 times before giving up
-    local i
-    for i in {1..5};
-        do go get -d && break || { echo "retrying build in 5s.."; sleep 5; } done
-    go build -o "${_EMERGE_ROOT}"/usr/bin/webhook
-    echo "done."
-}
 
 #
 # This hook is called just before starting the build of the root fs
@@ -35,5 +18,5 @@ configure_rootfs_build()
 #
 finish_rootfs_build()
 {
-    log_as_installed "manual install" "webhook-${_webhook_version}" "https://github.com/adnanh/webhook/"
+    :
 }
