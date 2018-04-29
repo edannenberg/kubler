9ba12accf3fe82ab4fc44be1391befa50503050e
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2018-04-29 17:40:46 +0200

    [s6] Use entr portage package

--- dock/kubler/images/s6/build.sh
+++ dock/kubler/images/s6/build.sh
@@ -1,8 +1,7 @@
 #
 # Kubler phase 1 config, pick installed packages and/or customize the build
 #
-_packages="sys-apps/s6"
-_entr_version="3.9"
+_packages="sys-apps/s6 app-admin/entr"
 
 #
 # This hook is called just before starting the build of the root fs
@@ -12,6 +11,7 @@ configure_rootfs_build()
     update_keywords 'dev-lang/execline' '+~amd64'
     update_keywords 'dev-libs/skalibs' '+~amd64'
     update_keywords 'sys-apps/s6' '+~amd64'
+    update_keywords 'app-admin/entr' '+~amd64'
 }
 
 #
@@ -22,13 +22,4 @@ finish_rootfs_build()
     # s6 folders
     cp -r /config/etc/* "${_EMERGE_ROOT}"/etc/
     mkdir -p "${_EMERGE_ROOT}"/service
-    # install entr
-    echo "building entr.."
-    wget "http://entrproject.org/code/entr-${_entr_version}.tar.gz"
-    tar xzf "entr-${_entr_version}.tar.gz"
-    cd eradman* && ./configure && make && make install
-    strip /usr/local/bin/entr
-    cp /usr/local/bin/entr "${_EMERGE_ROOT}"/bin
-    rm -rf /eradman* /entr*
-    log_as_installed "manual install" "entr-${_entr_version}" "http://entrproject.org/"
 }
