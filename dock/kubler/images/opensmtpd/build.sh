e037f188d5a2bccdae920b538e0d5e5a594887f6
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2018-12-11 05:17:07 +0100

    [opensmtpd] Default pid file location is now /var/run

--- dock/kubler/images/opensmtpd/build.sh
+++ dock/kubler/images/opensmtpd/build.sh
@@ -18,5 +18,5 @@ configure_rootfs_build()
 finish_rootfs_build()
 {
     sed -i 's/listen on localhost/listen on 0.0.0.0/g' "${_EMERGE_ROOT}"/etc/opensmtpd/smtpd.conf
-    mkdir "${_EMERGE_ROOT}"/run
+    mkdir "${_EMERGE_ROOT}"/var/run
 }
