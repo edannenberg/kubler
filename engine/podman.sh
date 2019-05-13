811049d4309bfdf387e0338d5abf9a9ddd5910de
babykart <babykart@gmail.com>
2019-05-14 01:12:33 +0200

    Add missing commit format in podman.sh #160
    Also adds an option to configure docker commit args.

--- engine/podman.sh
+++ engine/podman.sh
@@ -3,9 +3,11 @@
 # All rights reserved.
 
 PODMAN="${PODMAN:-podman}"
-PODMAN_BUILD_OPTS="${PODMAN_BUILD_OPTS:---format docker}"
+PODMAN_BUILD_OPTS="${PODMAN_BUILD_OPTS:---format=docker}"
+PODMAN_COMMIT_OPTS="${PODMAN_COMMIT_OPTS:-${PODMAN_BUILD_OPTS}}"
 DOCKER="${PODMAN}"
 DOCKER_BUILD_OPTS="${PODMAN_BUILD_OPTS}"
+DOCKER_COMMIT_OPTS="${PODMAN_COMMIT_OPTS}"
 
 # shellcheck source=engine/docker.sh
 source "${_KUBLER_DIR}/engine/docker.sh"
