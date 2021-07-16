47e1dd74f16b8f9bfafd2e07401184460a05229a
Berne Campbell <berne.campbell@gmail.com>
2021-07-16 12:11:55 +1000

    Add note about pipefail and use `sh`

--- template/docker/image/docker-healthcheck.sh
+++ template/docker/image/docker-healthcheck.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+# Note: pipefail is not supported in POSIX shell and will be silently ignored, unless bash is used
 set -eo pipefail
 
 # Do some tests and exit with either 0 for healthy or 1 for unhealthy
